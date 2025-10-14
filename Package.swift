576e3001a3c1c9e08c07480660ec6465dc9d2e6a
Conlan Ainsworth <csainsworth123@gmail.com>
2025-10-14 02:39:07 +0100

    Release v1.0.8 - Proper XCFramework structure with complete module support

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwifMetro",
-            url: "https://github.com/SwifMetro/SwifMetro/releases/download/v1.0.7/SwifMetro.xcframework.zip",
-            checksum: "d78755966b511d87cf6ceffbfeb3dfc9b626823fe875c0300c060b8f9db47c47"
+            url: "https://github.com/SwifMetro/SwifMetro/releases/download/v1.0.8/SwifMetro.xcframework.zip",
+            checksum: "299eec9a7a751cce005f832a5f994d96d1056828a87f2d89f9f329fb6709c0b8"
         )
     ]
 )
